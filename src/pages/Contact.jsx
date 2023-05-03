d1aff1e11b291b0bf5dd98ef566b6adaea07850e
Alexjandro Mendoza <alexjandro.mendoza21@gmail.com>
2023-05-03 12:38:40 -0400

    successfully added map to website. removed it from home page and added it to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,10 @@
 import Footer from '../minicomp/Footer'
 import {motion} from "framer-motion"
 import styled from "styled-components";
-
+import 'leaflet/dist/leaflet.css'
+import L from "leaflet";
+import React, { useMemo, useRef } from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
 
 const Bottom = styled.div`
@@ -11,11 +14,43 @@ width:100%;
 color: rgb(255, 255, 255);
 `;
 
+const Location = styled.div`
+position:fixed;
+bottom:20%;
+width:100%;
+
+`;
+
+const position = [40.928890, -73.894990]
+
+const icon = L.icon({
+  iconSize: [25, 41],
+  iconAnchor: [10, 41],
+  popupAnchor: [2, -40],
+  iconUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-icon.png",
+  shadowUrl: "https://unpkg.com/leaflet@1.6/dist/images/marker-shadow.png"
+});
+
 
 
 
 
 const Contact = () => {
+
+  const markerRef = useRef();
+
+  const eventHandlers = useMemo(
+    () => ({
+      mouseover() {
+        if (markerRef) markerRef.current.openPopup();
+      },
+      mouseout() {
+        if (markerRef) markerRef.current.closePopup();
+      }
+    }),
+    []
+  );
+  
 
 
 
@@ -44,13 +79,46 @@ const Contact = () => {
       
       </motion.div>
 
-
-      <motion.div
-        animate={{ x: 0, y: -70, }}
-        transition={{ ease: "easeOut", duration: 3 }}
-      >
-      </motion.div>
     </main>
+
+
+
+    <motion.div
+        animate={{ x: 0, y: 450, }}
+         transition={{ ease: "easeOut", duration: 2 }}
+      >
+
+
+
+    <Location>
+    <div className='leafletContainer'>
+          <MapContainer
+            style={{ height: '70%', width: '70%' }}
+            center={position}
+            zoom={14}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url='https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png'
+            />
+
+            <Marker 
+            ref={markerRef}
+            position={position}
+            icon={icon}
+            eventHandlers={eventHandlers}
+            >
+              <Popup> El Cazador Cafe</Popup>
+            </Marker>
+          </MapContainer>
+      </div>
+      </Location>
+      </motion.div>
+
+
+
+
 
       <Bottom> 
         <Footer />
